a241d1e7c68b7f3817ff50e0a3e0bada1b9f95fa
Aurèle <aurele.barriere@irisa.fr>
2023-05-16 15:15:57 +0200

    converting to np array

--- olinear/plot_exps.py
+++ olinear/plot_exps.py
@@ -5,7 +5,7 @@ import sys
 
 columns = ["Size", "OCaml", "JS"]
 data = pd.read_csv(sys.argv[1], names=columns)
-plt.plot(data.Size,data.OCaml,label="OCaml")
-plt.plot(data.Size,data.JS,label="JS")
+plt.plot(data.Size.values,data.OCaml.values,label="OCaml")
+plt.plot(data.Size.values,data.JS.values,label="JS")
 plt.legend(loc="upper left")
 plt.show()
