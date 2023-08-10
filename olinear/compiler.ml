401018684b23ef1c15b7ae7dc834f9dab37aaf6e
Aurèle <aurele.barriere@irisa.fr>
2023-08-10 16:44:37 +0200

    regex-linear-time compilation to bytecode (instead of quadratic due to list concatenation)

--- olinear/compiler.ml
+++ olinear/compiler.ml
@@ -10,9 +10,30 @@ open Bytecode
 (* defining registers corresponding to a given capture group *)
 let start_reg (c:capture) : register = 2 * c
 let end_reg (c:capture) : register = (2*c) + 1
+
+(** * Compilation data-structure  *)
+(* to avoid quadratic-time compilation by concatenating lists, we do the following: *)
+(* we build a tree of lists of instructions during compilation *)
+(* then we flatten this to a list of instructions *)
+(* finally, we'll convert this to an arry of instructions that will be used by the interpreter *)
+(* each of these steps should have linear time complexity in the regex *)
+
+type 'x treelist =
+  | Leaf of 'x list
+  | Concat of ('x treelist) * ('x treelist)
+
+let (@@) x y = Concat (x,y)
+
+(* transforms a treelist into a list *)
+(* linear time in the total number of 'x elements in t *)
+let rec tl_flatten (t:'x treelist) (tail:'x list): 'x list =
+  match t with
+  | Leaf l -> l@tail            (* here we will iterate over the elements of l *)
+  | Concat (t1,t2) ->
+     let l2 = tl_flatten t2 tail in
+     tl_flatten t1 l2
   
-  
-(** * Compilation  *)
+(** * Regex to Bytecode Compilation  *)
 (* currently, we are quadratic because of list concatenation *)
 (* our clearing of the registers and memory is also not optimal and can result in quadratic bytecode *)
 (* experimental V8 has the same issue *)
@@ -28,27 +49,27 @@ type comp_type =
 (* [fresh] is the next available instruction label *)
 (* also returns the next fresh label after compilation *)
 (* if the option progress is false, then this generates code that cannot make progress in the string *)
-let rec compile (r:regex) (fresh:label) (ctype:comp_type) : instruction list * label =
+let rec compile (r:regex) (fresh:label) (ctype:comp_type) : instruction treelist * label =
   match r with
-  | Re_empty -> ([], fresh)
+  | Re_empty -> (Leaf [], fresh)
   | Re_char ch ->
      begin match ctype with
-     | Progress -> ([Consume ch], fresh+1)
-     | ReconstructNulled -> ([Fail], fresh+1)
+     | Progress -> (Leaf [Consume ch], fresh+1)
+     | ReconstructNulled -> (Leaf [Fail], fresh+1)
      end
   | Re_dot ->
      begin match ctype with
-     | Progress -> ([ConsumeAll], fresh+1)
-     | ReconstructNulled -> ([Fail], fresh+1)
+     | Progress -> (Leaf [ConsumeAll], fresh+1)
+     | ReconstructNulled -> (Leaf [Fail], fresh+1)
      end
   | Re_con (r1, r2) ->
      let (l1, f1) = compile r1 fresh ctype in
      let (l2, f2) = compile r2 f1 ctype in
-     (l1 @ l2, f2)
+     (l1 @@ l2, f2)
   | Re_alt (r1, r2) ->
      let (l1, f1) = compile r1 (fresh+1) ctype in
      let (l2, f2) = compile r2 (f1+1) ctype in
-     ([Fork (fresh+1, f1+1)] @ l1 @ [Jmp f2] @ l2, f2)
+     (Leaf [Fork (fresh+1, f1+1)] @@ l1 @@ Leaf [Jmp f2] @@ l2, f2)
   | Re_quant (nul, qid, quant, r1) when ctype = Progress ->
      (* progress compilation, consuming is allowed *)
      begin match quant with
@@ -57,44 +78,44 @@ let rec compile (r:regex) (fresh:label) (ctype:comp_type) : instruction list * l
         | NonNullable ->
            (* no need for BeginLoop/EndLoop instructions *)
            let (l1, f1) = compile r1 (fresh+2) Progress in
-           ([Fork (fresh+1, f1+1); SetQuantToClock (qid,false)] @ l1 @ [Jmp fresh], f1+1)
+           (Leaf [Fork (fresh+1, f1+1); SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Jmp fresh], f1+1)
         | CINullable | CDNullable ->
            (* nullable case: BeginLoop/EndLoop instructions needed for JS empty quantification semantics *)
           let (l1, f1) = compile r1 (fresh+3) Progress in
-          ([Fork (fresh+1, f1+2); BeginLoop; SetQuantToClock (qid,false)] @ l1 @ [EndLoop; Jmp fresh], f1+2)
+          (Leaf [Fork (fresh+1, f1+2); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Jmp fresh], f1+2)
         end
      | LazyStar ->
         begin match nul with
         | NonNullable ->
            (* no need for BeginLoop/EndLoop instructions *)
            let (l1, f1) = compile r1 (fresh+2) Progress in
-           ([Fork (f1+1, fresh+1); SetQuantToClock (qid,false)] @ l1 @ [Jmp fresh], f1+1)
+           (Leaf [Fork (f1+1, fresh+1); SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Jmp fresh], f1+1)
         | CINullable | CDNullable ->
            (* nullable case: BeginLoop/EndLoop instructions needed for JS empty quantification semantics *)
            let (l1, f1) = compile r1 (fresh+3) Progress in
-           ([Fork (f1+2, fresh+1); BeginLoop; SetQuantToClock (qid,false)] @ l1 @ [EndLoop; Jmp fresh], f1+2)
+           (Leaf [Fork (f1+2, fresh+1); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Jmp fresh], f1+2)
         end
      | Plus ->
         begin match nul with
         | NonNullable ->
            let (l1, f1) = compile r1 (fresh+1) Progress in
-           ([SetQuantToClock (qid,false)] @ l1 @ [Fork (fresh,f1+1)], f1+1)
+           (Leaf [SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Fork (fresh,f1+1)], f1+1)
         | CINullable ->
            let (l1, f1) = compile r1 (fresh+3) Progress in
-           ([Fork (fresh+1,f1+2); BeginLoop; SetQuantToClock (qid,false)] @ l1 @ [EndLoop; Fork (fresh+1,f1+3); SetQuantToClock (qid,true)], f1+3)
+           (Leaf [Fork (fresh+1,f1+2); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Fork (fresh+1,f1+3); SetQuantToClock (qid,true)], f1+3)
         | CDNullable ->
            let (l1, f1) = compile r1 (fresh+3) Progress in
-           ([Fork (fresh+1,f1+2); BeginLoop; SetQuantToClock (qid,false)] @ l1 @ [EndLoop; Fork (fresh+1,f1+4); CheckNullable qid; SetQuantToClock (qid,true)], f1+4)
+           (Leaf [Fork (fresh+1,f1+2); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Fork (fresh+1,f1+4); CheckNullable qid; SetQuantToClock (qid,true)], f1+4)
         end
      | LazyPlus ->
         begin match nul with
         | NonNullable ->
            let (l1, f1) = compile r1 (fresh+1) Progress in
-           ([SetQuantToClock (qid,false)] @ l1 @ [Fork (f1+1,fresh)], f1+1)
+           (Leaf [SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Fork (f1+1,fresh)], f1+1)
         | _ -> 
            (* Compiling as a concatenation in the nullable case *)
            let (l1, f1) = compile (Re_con(r1,Re_quant(nul,qid,LazyStar,r1))) (fresh+1) Progress in
-           ([SetQuantToClock (qid,false)] @ l1, f1)
+           (Leaf [SetQuantToClock (qid,false)] @@ l1, f1)
         end
      end
   (* when ctype = ReconstrutNulled, ie we only want to find the top-priority nullable path *)
@@ -102,56 +123,56 @@ let rec compile (r:regex) (fresh:label) (ctype:comp_type) : instruction list * l
      begin match quant with
      | Star | LazyStar ->
         (* you can just skip the stars, there's no way to null them by going inside since they don't allow empty repetitions *)
-        ([],fresh)
+        (Leaf [],fresh)
      | Plus ->
         begin match nul with
-        | NonNullable -> ([Fail], fresh+1) (* you won't be able to null that expression *)
+        | NonNullable -> (Leaf [Fail], fresh+1) (* you won't be able to null that expression *)
         | CINullable ->                    
            (* recursively compile the inner nested + *)
            let (l1, f1) = compile r1 (fresh+1) ReconstructNulled in
-           ([SetQuantToClock (qid,true)] @ l1, f1)
+           (Leaf [SetQuantToClock (qid,true)] @@ l1, f1)
         | CDNullable ->         
            (* recursively compile the inner nested + *)
            let (l1, f1) = compile r1 (fresh+1) ReconstructNulled in
-           ([SetQuantToClock (qid,true)] @ l1, f1)
+           (Leaf [SetQuantToClock (qid,true)] @@ l1, f1)
         end
      | LazyPlus ->
         begin match nul with
-        | NonNullable -> ([Fail], fresh+1) (* you won't be able to null that expression *)            
+        | NonNullable -> (Leaf [Fail], fresh+1) (* you won't be able to null that expression *)            
         | _ ->
            let (l1, f1) = compile r1 (fresh+1) ctype in
            (* we don't need to reenter the star, compiling r1 once is enough *)
-           ([SetQuantToClock (qid,true)] @ l1, f1)
+           (Leaf [SetQuantToClock (qid,true)] @@ l1, f1)
         end
      end
   | Re_capture (cid, r1) ->
      let (l1, f1) = compile r1 (fresh+1) ctype in
-     ([SetRegisterToCP (start_reg cid)] @ l1 @ [SetRegisterToCP (end_reg cid)], f1+1)
+     (Leaf [SetRegisterToCP (start_reg cid)] @@ l1 @@ Leaf [SetRegisterToCP (end_reg cid)], f1+1)
   | Re_lookaround (lookid, looktype, r1) ->
      (* does not compile the lookarounds it depends on, only the current expression *)
      begin match looktype with
-     | Lookahead | Lookbehind -> ([CheckOracle lookid], fresh+1)
-     | NegLookahead | NegLookbehind -> ([NegCheckOracle lookid], fresh+1)
+     | Lookahead | Lookbehind -> (Leaf [CheckOracle lookid], fresh+1)
+     | NegLookahead | NegLookbehind -> (Leaf [NegCheckOracle lookid], fresh+1)
      end
 
 (* adds an accept at the end of the bytecode *)
 let compile_to_bytecode (r:regex) : code =
   let (c,_) = compile r 0 Progress in
-  let full_c = c @ [Accept] in
+  let full_c = tl_flatten c [Accept] in
   Array.of_list full_c
 
 (* same but with a WriteOracle instruction instead of an Accept *)
 (* l is the current lookid we are compiling the regex for *)
 let compile_to_write (r:regex) (l:lookid): code =
   let (c,_) = compile r 0 Progress in
-  let full_c = c @ [WriteOracle l] in
+  let full_c = tl_flatten c [WriteOracle l] in
   Array.of_list full_c
 
 (* compiles the bytecode for reconstructing the missing groups from nulled + *)
 (* this recursively compiles the nested + *)
 let compile_reconstruct_nulled (r:regex) : code =
   let (c,_) = compile r 0 ReconstructNulled in
-  let full_c = c @ [Accept] in
+  let full_c = tl_flatten c [Accept] in
   Array.of_list full_c
   
 
