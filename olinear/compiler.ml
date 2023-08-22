a8eca198c4b600fd3f0e4a4ad43635477e3a5509
Aurèle <aurele.barriere@irisa.fr>
2023-08-22 12:02:16 +0200

    compiling the new quantifiers. WIP

--- olinear/compiler.ml
+++ olinear/compiler.ml
@@ -67,81 +67,68 @@ let rec compile (r:regex) (fresh:label) (ctype:comp_type) : instruction treelist
      let (l1, f1) = compile r1 (fresh+1) ctype in
      let (l2, f2) = compile r2 (f1+1) ctype in
      (Leaf [Fork (fresh+1, f1+1)] @@ l1 @@ Leaf [Jmp f2] @@ l2, f2)
+     
   | Re_quant (nul, qid, quant, r1) when ctype = Progress ->
      (* progress compilation, consuming is allowed *)
-     begin match quant with
-     | Star ->
-        begin match nul with
-        | NonNullable ->
-           (* no need for BeginLoop/EndLoop instructions *)
-           let (l1, f1) = compile r1 (fresh+2) Progress in
-           (Leaf [Fork (fresh+1, f1+1); SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Jmp fresh], f1+1)
-        | CINullable | CDNullable ->
-           (* nullable case: BeginLoop/EndLoop instructions needed for JS empty quantification semantics *)
-          let (l1, f1) = compile r1 (fresh+3) Progress in
-          (Leaf [Fork (fresh+1, f1+2); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Jmp fresh], f1+2)
-        end
-     | LazyStar ->
-        begin match nul with
-        | NonNullable ->
-           (* no need for BeginLoop/EndLoop instructions *)
-           let (l1, f1) = compile r1 (fresh+2) Progress in
-           (Leaf [Fork (f1+1, fresh+1); SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Jmp fresh], f1+1)
-        | CINullable | CDNullable ->
-           (* nullable case: BeginLoop/EndLoop instructions needed for JS empty quantification semantics *)
-           let (l1, f1) = compile r1 (fresh+3) Progress in
-           (Leaf [Fork (f1+2, fresh+1); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Jmp fresh], f1+2)
-        end
-     | Plus ->
-        begin match nul with
-        | NonNullable ->
-           let (l1, f1) = compile r1 (fresh+1) Progress in
-           (Leaf [SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Fork (fresh,f1+1)], f1+1)
-        | CINullable ->
-           let (l1, f1) = compile r1 (fresh+3) Progress in
-           (Leaf [Fork (fresh+1,f1+2); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Fork (fresh+1,f1+3); SetQuantToClock (qid,true)], f1+3)
-        | CDNullable ->
-           let (l1, f1) = compile r1 (fresh+3) Progress in
-           (Leaf [Fork (fresh+1,f1+2); BeginLoop; SetQuantToClock (qid,false)] @@ l1 @@ Leaf [EndLoop; Fork (fresh+1,f1+4); CheckNullable qid; SetQuantToClock (qid,true)], f1+4)
-        end
-     | LazyPlus ->
-        begin match nul with
-        | NonNullable ->
-           let (l1, f1) = compile r1 (fresh+1) Progress in
-           (Leaf [SetQuantToClock (qid,false)] @@ l1 @@ Leaf [Fork (f1+1,fresh)], f1+1)
-        | _ -> 
-           (* Compiling as a concatenation in the nullable case *)
-           let (l1, f1) = compile (Re_con(r1,Re_quant(nul,qid,LazyStar,r1))) (fresh+1) Progress in
-           (Leaf [SetQuantToClock (qid,false)] @@ l1, f1)
-        end
-     end
+
+     (* there is some code duplication here, but it should help identify the different particular cases that we do linearly *)
+
+     (** particular case of the Non-Nullable +, where the last repetition can be used for the final loop *)
+     if (quant.min > 0 && quant.max = None && nul = NonNullable) then
+       (* repeat the body min-1 times *)
+       let (min_code, min_fresh) = repeat_min (quant.min-1) qid r1 fresh ctype in
+       (* loop on the last iteration *)
+       let (body_code, body_fresh) = compile r1 (min_fresh+1) ctype in
+       let fork = if quant.greedy then Fork (min_fresh, body_fresh+1)
+                  else Fork (body_fresh+1, min_fresh) in
+       (min_code @@ Leaf [SetQuantToClock (qid,false)] @@ body_code @@ Leaf [fork], body_fresh+1)
+       
+     (** particular case of the greedy CIN + *)
+     else if (quant.min > 0 && quant.max = None && nul = CINullable && quant.greedy) then
+       (* repeat the body min-1 times *)
+       let (min_code, min_fresh) = repeat_min (quant.min-1) qid r1 fresh ctype in
+       (* fork between the non-nullable repetition or the nulled branch *)
+       let (body_code, body_fresh) = compile r1 (min_fresh+3) ctype in
+       (min_code @@ Leaf [Fork (min_fresh+1, body_fresh+2); SetQuantToClock (qid, false); BeginLoop] @@ body_code @@ Leaf [EndLoop; Fork (min_fresh+1, body_fresh+3); SetQuantToClock (qid, true)],body_fresh+3)
+
+     (** particular case of the greedy CDN + *)
+     else if (quant.min > 0 && quant.max = None && nul = CDNullable && quant.greedy) then
+       (* repeat the body min-1 times *)
+       let (min_code, min_fresh) = repeat_min (quant.min-1) qid r1 fresh ctype in
+       (* fork between the non-nullable repetition or the nulled branch with a CDN test *)
+       let (body_code, body_fresh) = compile r1 (min_fresh+3) ctype in
+       (min_code @@ Leaf [Fork (min_fresh+1, body_fresh+2); SetQuantToClock (qid, false); BeginLoop] @@ body_code @@ Leaf [EndLoop; Fork (min_fresh+1, body_fresh+4); CheckNullable qid; SetQuantToClock (qid, true)],body_fresh+4)
+
+       
+     (** Generic Case  *)
+     else
+       (* first repeat the body min times *)
+       let (min_code, min_fresh) = repeat_min quant.min qid r1 fresh ctype in
+       begin match quant.max with
+       | None ->
+          (* create a loop for the following repetitions *)
+          (* iterations of this loop cannot match the empty string, hence the BeginLoop/EndLoop instructions *)
+          (* TODO possible optimization: in the non-nullable case, I could remove BeginLoop/EndLoop *)
+          let (iter_code, iter_fresh) = compile r1 (min_fresh+3) ctype in
+          let fork = if quant.greedy then Fork(min_fresh+1, iter_fresh+2)
+                     else Fork (iter_fresh+2,min_fresh+1) in
+          (min_code @@ Leaf [fork; SetQuantToClock (qid, false); BeginLoop] @@ iter_code @@ Leaf [EndLoop; Jmp min_fresh],iter_fresh+2)
+       | Some max ->
+          (* repeat the optional repetitions max-min times *)
+          let (opt_code, opt_fresh) = repeat_optional (max-quant.min) qid r1 min_fresh ctype quant.greedy in
+          (min_code @@ opt_code,opt_fresh)
+       end
+
   (* when ctype = ReconstrutNulled, ie we only want to find the top-priority nullable path *)
   | Re_quant (nul, qid, quant, r1) ->
-     begin match quant with
-     | Star | LazyStar ->
-        (* you can just skip the stars, there's no way to null them by going inside since they don't allow empty repetitions *)
-        (Leaf [],fresh)
-     | Plus ->
-        begin match nul with
-        | NonNullable -> (Leaf [Fail], fresh+1) (* you won't be able to null that expression *)
-        | CINullable ->                    
-           (* recursively compile the inner nested + *)
-           let (l1, f1) = compile r1 (fresh+1) ReconstructNulled in
-           (Leaf [SetQuantToClock (qid,true)] @@ l1, f1)
-        | CDNullable ->         
-           (* recursively compile the inner nested + *)
-           let (l1, f1) = compile r1 (fresh+1) ReconstructNulled in
-           (Leaf [SetQuantToClock (qid,true)] @@ l1, f1)
-        end
-     | LazyPlus ->
-        begin match nul with
-        | NonNullable -> (Leaf [Fail], fresh+1) (* you won't be able to null that expression *)            
-        | _ ->
-           let (l1, f1) = compile r1 (fresh+1) ctype in
-           (* we don't need to reenter the star, compiling r1 once is enough *)
-           (Leaf [SetQuantToClock (qid,true)] @@ l1, f1)
-        end
-     end
+     if (quant.min = 0) then (Leaf [], fresh) (* optional repetitions can't consume the empty string, so skip it *)
+     else if (nul = NonNullable) then (Leaf [Fail], fresh+1) (* you won't be able to null that expression *)
+     else
+       (* in the case where min>0, and the body might be nullable  *)
+       (* we only have to compile one iteration, since only the last iteration matters and iterations don't consume *)
+       let (l1, f1) = compile r1 (fresh+1) ReconstructNulled in
+       (Leaf [SetQuantToClock (qid,true)] @@ l1, f1)
+         
   | Re_capture (cid, r1) ->
      let (l1, f1) = compile r1 (fresh+1) ctype in
      (Leaf [SetRegisterToCP (start_reg cid)] @@ l1 @@ Leaf [SetRegisterToCP (end_reg cid)], f1+1)
@@ -153,6 +140,28 @@ let rec compile (r:regex) (fresh:label) (ctype:comp_type) : instruction treelist
      end
   | Re_anchor a -> (Leaf [AnchorAssertion a], fresh+1)
 
+(* repeats a [r] regex [min] times when it's inside a quantifier [qid] with minimum repetitions *)
+and repeat_min (min:int) (qid:quantid) (r:regex) (fresh:label) (ctype:comp_type) : instruction treelist * label =
+  (* TODO possible optimization: there's no need to capture/set anything on the first min-1 repetitions *)
+  if min = 0 then (Leaf [], fresh)
+  else
+    let (body_code, new_fresh) = compile r (fresh+1) ctype in
+    let (next_code, next_fresh) = repeat_min (min-1) qid r new_fresh ctype in
+    (Leaf [SetQuantToClock (qid,false)] @@ body_code @@ next_code, next_fresh)
+
+(* repeats the optional max-min repetitions of a regex inside a bounded quantifier *)
+(* those repetitions are not allowed to match the empty string, hence the BeginLoop/EndLoop instructions *)
+and repeat_optional (nb:int) (qid:quantid) (r:regex) (fresh:label) (ctype:comp_type) (greedy:bool) : instruction treelist * label =
+  if nb = 0 then (Leaf [], fresh)
+  else
+    let (body_code, new_fresh) = compile r (fresh+3) ctype in
+    let (next_code, next_fresh) = repeat_optional (nb-1) qid r (new_fresh+1) ctype greedy in
+    let fork = if greedy then Fork(fresh+1,next_fresh+1)
+               else Fork(next_fresh+1,fresh+1) in
+    (Leaf [fork; SetQuantToClock (qid,false); BeginLoop] @@ body_code @@ Leaf [EndLoop] @@ next_code,next_fresh+1)
+  
+    
+                 
 (* adds an accept at the end of the bytecode *)
 let compile_to_bytecode (r:regex) : code =
   let (c,_) = compile r 0 Progress in
