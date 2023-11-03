d0b16bbff814f8fba8be334fec4c23d1c39efc80
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2023-11-03 17:53:17 +0100

    Move benchmark vectors to a separate file

--- benchmark.ml
+++ benchmark.ml
@@ -11,7 +11,7 @@ open Unix
 open Gc
 open Flags
 open Arg
-
+open Benchmark_vectors
 
 (* path to V8 executable *)
 (* this V8 executable needs to be patched in two ways: *)
@@ -35,21 +35,6 @@ let irregexptimer = "scripts_bench/irregexptimer.js"
 let warmups = ref 10
 (* the number of times we perform each test *)
 let repetitions = ref 10
-
-type engine =
-  | OCaml
-  | OldV8Linear
-  | NewV8Linear
-  | Irregexp
-  | LinearBaseline
-
-let engine_name (e:engine) : string =
-  match e with
-  | OCaml -> "OCaml"
-  | OldV8Linear -> "oldV8L"
-  | NewV8Linear -> "newV8L"
-  | Irregexp -> "Irregexp"
-  | LinearBaseline -> "LinearBaseline"
 
 
 (* calling the matcher.native executable *)
@@ -86,44 +71,13 @@ let get_time_irregexp (r:raw_regex) (str:string): string =
 let get_time (e:engine) (r:raw_regex) (str:string) : string =
   match e with
   | OCaml -> get_time_ocaml "./matcher.native" r str
+  | OCamlBench -> failwith "get_time incompatible with OCamlBench engine"
   | OldV8Linear -> get_time_oldv8linear r str
   | NewV8Linear -> get_time_newv8linear r str
   | Irregexp -> get_time_irregexp r str
   | LinearBaseline -> get_time_ocaml "./linearbaseline.native" r str
 
 
-(* an engine configuration to test *)
-type engine_conf =
-  { eng: engine;                (* which engine *)
-    min_size: int;              (* minimum size of the benchmark *)
-    max_size: int               (* maximum size of the benchmark *)
-  }
-
-              
-(* A benchmark where we vary the length of the regex *)
-type regex_benchmark =
-  { name: string;
-    confs: engine_conf list;       (* list of all engine configurations to test *)
-    param_regex: int -> raw_regex; (* the family of regexes *)
-    input_str: string;             (* the string on which to match *)
-  }
-
-type string_benchmark =
-  { name: string;
-    confs: engine_conf list;
-    param_str: int -> string;
-    rgx: raw_regex;
-  }
-
-type benchmark =
-  | RB of regex_benchmark
-  | SB of string_benchmark
-
-let bench_name (b:benchmark) : string =
-  match b with
-  | RB r -> r.name
-  | SB s -> s.name
-        
 (* runs a regex-size benchmark on a single engine and prints the result to a csv file *)
 let run_regex_config (ec:engine_conf) (param_regex:int->raw_regex) (str:string) (name:string) : unit =
   Printf.printf "Testing engine %s:\n%!" (engine_name ec.eng);
@@ -138,9 +92,12 @@ let run_regex_config (ec:engine_conf) (param_regex:int->raw_regex) (str:string) 
   Printf.printf "\n%!";
   Unix.sleep 1
 
-(* runs a string-size benchmark on a single engine and prints the result to a csv file *)
-let run_string_config (ec:engine_conf) (param_str:int->string) (reg:raw_regex) (name:string) : unit =
-  Printf.printf "Testing engine %s:\n%!" (engine_name ec.eng);
+(* TODO add regex range in addition to str range in single function *)
+let run_ocamlbench_config (ec:engine_conf) (param_str:int->string) (reg:raw_regex) (name:string) : unit =
+  failwith "TODO"
+
+
+let run_simple_config (ec:engine_conf) (param_str:int->string) (reg:raw_regex) (name:string) : unit =
   let oc = open_out (bench_dir^name^"_"^(engine_name ec.eng)^".csv") in
   for i = ec.min_size to ec.max_size do
     Printf.printf " %s\r%!" (string_of_int i); (* live update *)
@@ -152,7 +109,16 @@ let run_string_config (ec:engine_conf) (param_str:int->string) (reg:raw_regex) (
   Printf.printf "\n%!";
   Unix.sleep 1
 
-  
+
+(* runs a string-size benchmark on a single engine and prints the result to a csv file *)
+let run_string_config (ec:engine_conf) (param_str:int->string) (reg:raw_regex) (name:string) : unit =
+  Printf.printf "Testing engine %s:\n%!" (engine_name ec.eng);
+  match ec.eng with
+  | OCamlBench ->
+     run_ocamlbench_config ec (param_str:int->string) (reg:raw_regex) (name:string)
+  | _ ->
+     run_simple_config ec (param_str:int->string) (reg:raw_regex) (name:string)
+
 let run_regex_benchmark (rb:regex_benchmark) : unit =
   Printf.printf ("Generating .csv files for benchmark %s:\n") (rb.name);
   List.iter (fun rc -> run_regex_config rc rb.param_regex rb.input_str rb.name) rb.confs
@@ -162,185 +128,15 @@ let run_string_benchmark (sb:string_benchmark) : unit =
   List.iter (fun rc -> run_string_config rc sb.param_str sb.rgx sb.name) sb.confs
 
 
-(** * Defining benchmarks *)
 
-
-(** * Nested Non-Nullable Plus  *)
-(* r0 = a *)
-(* rn = rn-1+ *)
-let rec nested_nn_plus_reg = fun reg_size ->
-  match reg_size with
-  | 0 -> raw_char('a')
-  | _ -> raw_plus(nested_nn_plus_reg (reg_size - 1))
-
-let nested_nn_plus_string = String.make 100 'a'
-
-let nn_plus_confs =
-  [ {eng=NewV8Linear; min_size=0; max_size=500 };
-    {eng=OldV8Linear; min_size=0; max_size=20 } ]
-
-let nested_nn_plus : regex_benchmark =
-  { name = "NNPlus";
-    confs = nn_plus_confs;
-    param_regex = nested_nn_plus_reg;
-    input_str = nested_nn_plus_string;
-  }
-
-(** * Nested CDN Plus  *)
-(* I picked an example with anchors rather than lookarounds *)
-(* so that we can compare the results to V8Linear *)
-(* I picked an example where all CDNs end up nulled and *)
-(* All CDNs need to be launched again to reconstruct a group *)
-
-(* r0 = a|(^) *)
-(* rn = rn-1+ *)
-let rec nested_cdn_reg = fun reg_size ->
-  match reg_size with
-  | 0 -> Raw_alt(raw_char('a'),Raw_capture(Raw_anchor(BeginInput)))
-  | _ -> raw_plus(nested_cdn_reg (reg_size - 1))
-
-let nested_cdn_string = "b"
-
-let nested_cdn_confs =
-  [ {eng=OCaml; min_size=0; max_size=500 };
-    {eng=OldV8Linear; min_size=0; max_size=20 } ]
-
-let nested_cdn : regex_benchmark =
-  { name = "CDN";
-    confs = nested_cdn_confs;
-    param_regex = nested_cdn_reg;
-    input_str = nested_cdn_string;
-  }
-  
-
-(** * Clocks have a better complexity than dynamic clearing of registers  *)
-(* r0 = . *)
-(* rn = (rn-1)* *)
-let rec clocks_reg = fun reg_size ->
-  match reg_size with
-  | 0 -> raw_char('a')
-  | _ -> raw_star(Raw_capture(clocks_reg (reg_size - 1)))
-
-let clocks_string = String.make 100 'a'
-
-let clocks_conf =
-  [ {eng=OCaml; min_size=0; max_size=200 };
-    {eng=OldV8Linear; min_size=0; max_size=150 } ]
-
-let clocks : regex_benchmark =
-  { name = "Clocks";
-    confs = clocks_conf;
-    param_regex = clocks_reg;
-    input_str = clocks_string;
-  }
-
-(** * Lookarounds Regex-Size *)
-(* r0 = ( a* )b *)
-(* rn = a (?= rn-1 ) *)
-let rec nested_look_reg = fun reg_size ->
-  match reg_size with
-  | 0 -> Raw_con(Raw_capture(raw_star(raw_char('a'))),raw_char('b'))
-  | _ -> Raw_con(raw_char('a'),Raw_lookaround(Lookahead,nested_look_reg (reg_size -1)))
-       
-let nested_look_reg_str = String.make 100 'a' ^ "b"
-
-let nested_look_conf =
-  [ {eng=OCaml; min_size=0; max_size=300 };
-    {eng=Irregexp; min_size=0; max_size=300 } ]
-
-let nested_lookarounds : regex_benchmark =
-  { name = "LAreg";
-    confs = nested_look_conf;
-    param_regex = nested_look_reg;
-    input_str = nested_look_reg_str;
-  }
-
-(** * Lookarounds String-Size  *)
-(* (?: a (?= a* b) )* *)
-let nested_la_reg = raw_star(Raw_con(raw_char('a'),Raw_lookaround(Lookahead,Raw_con(raw_star(raw_char('a')),raw_char('b')))))
-
-let nested_la_param_str = fun str_size ->
-  String.make str_size 'a' ^ "b"
-
-let nested_look_str_conf =
-  [ {eng=OCaml; min_size=0; max_size=3000 };
-    {eng=Irregexp; min_size=0; max_size=3000 } ]
-
-let nested_lookarounds_string : string_benchmark =
-  { name = "LAstr";
-    confs = nested_look_str_conf;
-    param_str = nested_la_param_str;
-    rgx = nested_la_reg;
-  }
-
-
-(** * Lookbehinds String-Size  *)
-(* b(?: a (?<= a* b) )* *)
-let nested_lb_reg = Raw_con(raw_char('b'),raw_star(Raw_con(raw_char('a'),Raw_lookaround(Lookbehind,Raw_con(raw_char('b'),raw_star(raw_char('a')))))))
-
-let nested_lb_param_str = fun str_size ->
-  "b" ^ String.make str_size 'a'
-
-let nested_lookb_str_conf =
-  [ {eng=LinearBaseline; min_size=0; max_size=3000 };
-    {eng=OCaml; min_size=0; max_size=3000 };
-    {eng=NewV8Linear; min_size=0; max_size=3000 };
-    {eng=Irregexp; min_size=0; max_size=3000 } ]
-
-let nested_lookbehinds_string : string_benchmark =
-  { name = "LBstr";
-    confs = nested_lookb_str_conf;
-    param_str = nested_lb_param_str;
-    rgx = nested_lb_reg;
-  }
-                  
-  
-  
-(** * Register Data-Structure Benchmark  *)
-(* The goal is to launch this benchmark several times, switching the register implementation in the interpreter *)
-(* Later I'll make the interpreter a functor to switch dynamically, here you need to recompile each time. *)
-(* we'll compare the results of each register data_structure (ds) *)
-
-let rec ds_reg = fun reg_size ->
-  match reg_size with
-  | 0 -> raw_qmark(Raw_capture(raw_char('a')))
-  | _ -> Raw_con(raw_qmark(Raw_capture(raw_char('a'))),ds_reg (reg_size - 1))
-
-let ds_param_reg = fun reg_size ->
-  raw_star(ds_reg reg_size)
-
-let ds_str = String.make 1000 'a'
-
-let ds_conf =
-  [{eng=OCaml; min_size=0; max_size=100}]
-
-let ds : regex_benchmark =
-  { name = Interpreter.Regs.name;
-    confs = ds_conf;
-    param_regex = ds_param_reg;
-    input_str = ds_str }
-  
 
 (** * Benchmark Executable  *)
-  
-let all_bench : benchmark list =
-  [RB nested_nn_plus; RB nested_cdn; RB clocks;
-   RB nested_lookarounds; SB nested_lookarounds_string;
-   SB nested_lookbehinds_string;
-   RB ds]
 
-let bench_names = List.map (fun b -> bench_name b) all_bench
-let bench_names_string =
-  List.fold_left (fun a b -> a ^ " " ^ b) "" bench_names
+let exec_bench (bench: benchmark) : unit =
+  match bench with
+  | RB b -> run_regex_benchmark b
+  | SB b -> run_string_benchmark b
 
-let exec_bench (name:string) : unit =
-  try
-    let bench = List.find (fun b -> bench_name b = name) all_bench in
-    match bench with
-    | RB b -> run_regex_benchmark b
-    | SB b -> run_string_benchmark b
-  with Not_found -> Printf.printf "Couldn't find benchmark %s\nAvailable benchmarks: %s\n" name bench_names_string
-  
                   
 let main =
   debug := false;
@@ -357,5 +153,4 @@ let main =
   let usage = "./benchmark.native [-oldv8 path_to_old_d8] [-newv8 path_to_new_d8] [-warmups 10] [-repet 10] benchmark list\n" in
   let full_usage = usage ^ "\nAvailable benchmarks: " ^ bench_names_string in
   Arg.parse speclist (fun s -> bench_list := s::!bench_list) full_usage;
-  List.iter exec_bench !bench_list
-    
+  List.iter exec_bench (List.map benchmark_vector_of_string !bench_list)
---
+++ benchmark_vectors.ml
@@ -0,0 +1,227 @@
+open Regex
+
+type engine =
+  | OCaml
+  | OCamlBench
+  | OldV8Linear
+  | NewV8Linear
+  | Irregexp
+  | LinearBaseline
+
+let engine_name (e:engine) : string =
+  match e with
+  | OCaml -> "OCaml"
+  | OCamlBench -> "OCamlBench"
+  | OldV8Linear -> "oldV8L"
+  | NewV8Linear -> "newV8L"
+  | Irregexp -> "Irregexp"
+  | LinearBaseline -> "LinearBaseline"
+
+(* an engine configuration to test *)
+type engine_conf =
+  { eng: engine;                (* which engine *)
+    min_size: int;              (* minimum size of the benchmark *)
+    max_size: int               (* maximum size of the benchmark *)
+  }
+
+
+(* A benchmark where we vary the length of the regex *)
+type regex_benchmark =
+  { name: string;
+    confs: engine_conf list;       (* list of all engine configurations to test *)
+    param_regex: int -> raw_regex; (* the family of regexes *)
+    input_str: string;             (* the string on which to match *)
+  }
+
+type string_benchmark =
+  { name: string;
+    confs: engine_conf list;
+    param_str: int -> string;
+    rgx: raw_regex;
+  }
+
+type benchmark =
+  | RB of regex_benchmark
+  | SB of string_benchmark
+
+let bench_name (b:benchmark) : string =
+  match b with
+  | RB r -> r.name
+  | SB s -> s.name
+
+(** * Defining benchmarks *)
+
+(** * Nested Non-Nullable Plus  *)
+(* r0 = a *)
+(* rn = rn-1+ *)
+let rec nested_nn_plus_reg = fun reg_size ->
+  match reg_size with
+  | 0 -> raw_char('a')
+  | _ -> raw_plus(nested_nn_plus_reg (reg_size - 1))
+
+let nested_nn_plus_string = String.make 100 'a'
+
+let nn_plus_confs =
+  [ {eng=NewV8Linear; min_size=0; max_size=500 };
+    {eng=OldV8Linear; min_size=0; max_size=20 } ]
+
+let nested_nn_plus : regex_benchmark =
+  { name = "NNPlus";
+    confs = nn_plus_confs;
+    param_regex = nested_nn_plus_reg;
+    input_str = nested_nn_plus_string;
+  }
+
+(** * Nested CDN Plus  *)
+(* I picked an example with anchors rather than lookarounds *)
+(* so that we can compare the results to V8Linear *)
+(* I picked an example where all CDNs end up nulled and *)
+(* All CDNs need to be launched again to reconstruct a group *)
+
+(* r0 = a|(^) *)
+(* rn = rn-1+ *)
+let rec nested_cdn_reg = fun reg_size ->
+  match reg_size with
+  | 0 -> Raw_alt(raw_char('a'),Raw_capture(Raw_anchor(BeginInput)))
+  | _ -> raw_plus(nested_cdn_reg (reg_size - 1))
+
+let nested_cdn_string = "b"
+
+let nested_cdn_confs =
+  [ {eng=OCaml; min_size=0; max_size=500 };
+    {eng=OldV8Linear; min_size=0; max_size=20 } ]
+
+let nested_cdn : regex_benchmark =
+  { name = "CDN";
+    confs = nested_cdn_confs;
+    param_regex = nested_cdn_reg;
+    input_str = nested_cdn_string;
+  }
+
+
+(** * Clocks have a better complexity than dynamic clearing of registers  *)
+(* r0 = . *)
+(* rn = (rn-1)* *)
+let rec clocks_reg = fun reg_size ->
+  match reg_size with
+  | 0 -> raw_char('a')
+  | _ -> raw_star(Raw_capture(clocks_reg (reg_size - 1)))
+
+let clocks_string = String.make 100 'a'
+
+let clocks_conf =
+  [ {eng=OCaml; min_size=0; max_size=200 };
+    {eng=OldV8Linear; min_size=0; max_size=150 } ]
+
+let clocks : regex_benchmark =
+  { name = "Clocks";
+    confs = clocks_conf;
+    param_regex = clocks_reg;
+    input_str = clocks_string;
+  }
+
+(** * Lookarounds Regex-Size *)
+(* r0 = ( a* )b *)
+(* rn = a (?= rn-1 ) *)
+let rec nested_look_reg = fun reg_size ->
+  match reg_size with
+  | 0 -> Raw_con(Raw_capture(raw_star(raw_char('a'))),raw_char('b'))
+  | _ -> Raw_con(raw_char('a'),Raw_lookaround(Lookahead,nested_look_reg (reg_size -1)))
+
+let nested_look_reg_str = String.make 100 'a' ^ "b"
+
+let nested_look_conf =
+  [ {eng=OCaml; min_size=0; max_size=300 };
+    {eng=Irregexp; min_size=0; max_size=300 } ]
+
+let nested_lookarounds : regex_benchmark =
+  { name = "LAreg";
+    confs = nested_look_conf;
+    param_regex = nested_look_reg;
+    input_str = nested_look_reg_str;
+  }
+
+(** * Lookarounds String-Size  *)
+(* (?: a (?= a* b) )* *)
+let nested_la_reg = raw_star(Raw_con(raw_char('a'),Raw_lookaround(Lookahead,Raw_con(raw_star(raw_char('a')),raw_char('b')))))
+
+let nested_la_param_str = fun str_size ->
+  String.make str_size 'a' ^ "b"
+
+let nested_look_str_conf =
+  [ {eng=OCaml; min_size=0; max_size=3000 };
+    {eng=Irregexp; min_size=0; max_size=3000 } ]
+
+let nested_lookarounds_string : string_benchmark =
+  { name = "LAstr";
+    confs = nested_look_str_conf;
+    param_str = nested_la_param_str;
+    rgx = nested_la_reg;
+  }
+
+
+(** * Lookbehinds String-Size  *)
+(* b(?: a (?<= a* b) )* *)
+let nested_lb_reg = Raw_con(raw_char('b'),raw_star(Raw_con(raw_char('a'),Raw_lookaround(Lookbehind,Raw_con(raw_char('b'),raw_star(raw_char('a')))))))
+
+let nested_lb_param_str = fun str_size ->
+  "b" ^ String.make str_size 'a'
+
+let nested_lookb_str_conf =
+  [ {eng=LinearBaseline; min_size=0; max_size=3000 };
+    {eng=OCaml; min_size=0; max_size=3000 };
+    {eng=OCamlBench; min_size=0; max_size=3000 };
+    {eng=NewV8Linear; min_size=0; max_size=3000 };
+    {eng=Irregexp; min_size=0; max_size=3000 } ]
+
+let nested_lookbehinds_string : string_benchmark =
+  { name = "LBstr";
+    confs = nested_lookb_str_conf;
+    param_str = nested_lb_param_str;
+    rgx = nested_lb_reg;
+  }
+
+
+
+(** * Register Data-Structure Benchmark  *)
+(* The goal is to launch this benchmark several times, switching the register implementation in the interpreter *)
+(* Later I'll make the interpreter a functor to switch dynamically, here you need to recompile each time. *)
+(* we'll compare the results of each register data_structure (ds) *)
+
+let rec ds_reg = fun reg_size ->
+  match reg_size with
+  | 0 -> raw_qmark(Raw_capture(raw_char('a')))
+  | _ -> Raw_con(raw_qmark(Raw_capture(raw_char('a'))),ds_reg (reg_size - 1))
+
+let ds_param_reg = fun reg_size ->
+  raw_star(ds_reg reg_size)
+
+let ds_str = String.make 1000 'a'
+
+let ds_conf =
+  [{eng=OCaml; min_size=0; max_size=100}]
+
+let ds : regex_benchmark =
+  { name = Interpreter.Regs.name;
+    confs = ds_conf;
+    param_regex = ds_param_reg;
+    input_str = ds_str }
+
+
+let all_bench : benchmark list =
+  [RB nested_nn_plus; RB nested_cdn; RB clocks;
+   RB nested_lookarounds; SB nested_lookarounds_string;
+   SB nested_lookbehinds_string;
+   RB ds]
+
+let bench_names = List.map (fun b -> bench_name b) all_bench
+let bench_names_string =
+  List.fold_left (fun a b -> a ^ " " ^ b) "" bench_names
+
+let benchmark_vector_of_string name =
+  try
+    List.find (fun b -> bench_name b = name) all_bench
+  with Not_found -> (
+    Printf.printf "Couldn't find benchmark %s\nAvailable benchmarks: %s\n" name bench_names_string;
+    exit 1
+  )
