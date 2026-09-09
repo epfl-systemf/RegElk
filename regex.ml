779c1cf0909144708da1a0370f18a7627a075a65
rayhaneh.einollahi <rayhaneheinollahi@gmail.com>
2026-09-09 16:52:51 +0200

    Fix annotating regex to have same capture id and regex for both instances

--- regex.ml
+++ regex.ml
@@ -305,14 +305,15 @@ let lazy_prefix (r:regex) : regex =
   | Re_lookaround of lookid * lookaround * regex *)
 let annotate_to_clemele (ra:raw_regex) : regex =
   (* [^]*? (?: (r) [^]*? )* (?![^]) (?:(r)|) *)
-  let (r1, c1, l1, q1)  = annotate_regex ra 2 2 3 in
-  let (r2, _, _, _)  = annotate_regex ra c1 l1 q1 in
+  let (r1, _, _, _)  = annotate_regex ra 1 2 3 in
+  (* let (r2, _, _, _)  = annotate_regex ra c1 l1 q1 in *)
   let lazy_part1 = Re_quant(NonNullable, 0, {min=0;max=None;greedy=false}, Re_character (NegClass [])) in
   let lazy_part2 = Re_quant(NonNullable, 1, {min=0;max=None;greedy=false}, Re_character (NegClass [])) in
-
+  (* I am using the same capture group id for both instances of the ra regex 'r1' *)
+  (* also the same regex is used too hope it doesnt break the code! *)
   let iteration_part = Re_quant(NonNullable, 2, {min=0;max=None;greedy=true}, Re_con (Re_capture (0, r1),lazy_part2) ) in
   let terminating_part = Re_con(Re_lookaround(1,NegLookahead, Re_character (NegClass [])),
-                                Re_alt(Re_capture(1,r2), Re_empty)) in
+                                Re_alt(Re_capture(0,r1), Re_empty)) in
   
   Re_con(lazy_part1, Re_con(iteration_part,terminating_part))
 
